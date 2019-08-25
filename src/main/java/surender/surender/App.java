0201062e1887ff391b1a35033adca39e682ed5ef
surender <msurender85@gmail.com>
2019-08-25 13:35:11 -0700

    first commit

---
+++ src/main/java/surender/surender/App.java
@@ -0,0 +1,13 @@
+package surender.surender;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
